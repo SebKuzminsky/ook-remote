7dcdf57891f6750eee588b14cdb14b6be11eae4e
Sebastian Kuzminsky <seb@highlab.com>
2023-11-08 21:26:32 -0700

    read and process commands over USB serial port
    So far it just knows "txN", to transmit the 32-bit number N.

--- firmware/main.cpp
+++ firmware/main.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <ctype.h>
 #include <string.h>
 #include <stdlib.h>
 
@@ -8,11 +9,52 @@
 #include "ook.pio.h"
 
 
+// Rising edge on this GPIO indicates beginning of a transmission,
+// used to trigger a scope for debugging.
+uint const debug_gpio = 1;
+
+
+// Process commands received from the computer via USB serial.
+// Valid input strings:
+//     'txN': N is a 32-bit hex string.  Transmit the hex string.
+void process_input(char const * in) {
+    if (strncasecmp("tx", in, 2) == 0) {
+        uint32_t data = 0;
+        sscanf(&in[2], "%lx", &data);
+        printf("tx 0x%08lx\n", data);
+        gpio_put(debug_gpio, 1);
+        ook_send(data);
+        gpio_put(debug_gpio, 0);
+    } else {
+        printf("unknown input '%s'\n", in);
+    }
+}
+
+
+void read_serial(void) {
+    static char in[16];
+    static int index = 0;
+
+    // Block waiting for an input character.
+    in[index] = getchar();
+    if (isspace(in[index])) {
+        in[index] = '\0';
+        process_input(in);
+        index = 0;
+        return;
+    }
+    ++index;
+    if (index == sizeof(in)) {
+        printf("input buffer overflow, discarding input '%s'\n", in);
+        index = 0;
+    }
+}
+
+
 int main() {
     PIO pio = pio0;
     uint const ook_gpio = 0;
     uint const ook_microseconds_per_bit = 625;
-    uint const debug_gpio = 1;
 
     stdio_init_all();
     clocks_init();
@@ -29,12 +71,7 @@ int main() {
     uint ook_offset = pio_add_program(pio, &ook_program);
     ook_init(pio, ook_offset, ook_gpio, ook_microseconds_per_bit);
 
-    uint32_t data = 0x1234567f;
     for (;;) {
-        printf("sending data: 0x%08lx\n", data);
-        gpio_put(debug_gpio, 1);
-        ook_send(data);
-        gpio_put(debug_gpio, 0);
-        ++data;
+        read_serial();
     }
 }
